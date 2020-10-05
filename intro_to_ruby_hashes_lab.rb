8d61fd2b39bf3944b016a0e341431ff53b7f7bc3
Mehdi A Noorani <noorani.mehdi@outlook.com>
2020-10-05 20:40:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,11 @@ def base_hash
   # keyword "return." Nice and neat.
   
   base_hash = {
-    :railroads => { } 
+    :railroads => {
+      :pieces => 4, 
+      :rent_in_dollars => { }, 
+      :names => { }
+    } 
   }
 
 end
@@ -14,9 +18,7 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   
-   base_hash = {
-    :railroads => { } 
-  }
+  
 
 end
 
